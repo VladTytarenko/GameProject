3d04db61994b17abf6253216da832afcde59af77
Vlad Tytarenko <tytarenkovlad@outlook.com>
2017-06-26 18:50:02 +0300

    test

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Escape) && gameObject.activeInHierarchy)
+        //{ // KeyCode.return
+            //if(gameObject.activeInHierarchy == true)
+            Application.LoadLevel(0);
+        //}
+
+	}
+}
